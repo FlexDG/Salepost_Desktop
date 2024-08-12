865fc114c8529dff149afec8c4c6af68d8e91e0c
Felix Rafael De Guzman <deguzman.felixrafael@auf.edu.ph>
2024-08-13 07:11:28 +0800

    day 5 salepost desktop update

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@
  * navbar toggle
  */
 
+
+
 const overlay = document.querySelector("[data-overlay]");
 const navOpenBtn = document.querySelector("[data-nav-open-btn]");
 const navbar = document.querySelector("[data-navbar]");
